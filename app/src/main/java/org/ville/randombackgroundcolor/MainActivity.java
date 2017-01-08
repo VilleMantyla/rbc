1a1f73f3b116978f1386cab244f710da88ce93da
ville <vianman>
2017-01-08 17:20:31 +0200

    Added a button that changes phone's background
    Pressing a button changes phone's background to
    the background color of the main activity.
    
    Set the ImageView be clickable in the xml file and removed the
    corresponding code from the java file.

--- app/src/main/java/org/ville/randombackgroundcolor/MainActivity.java
+++ app/src/main/java/org/ville/randombackgroundcolor/MainActivity.java
@@ -1,12 +1,19 @@
 package org.ville.randombackgroundcolor;
 
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,12 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         backgroundImageView = (ImageView) findViewById(R.id.background_color);
-        backgroundImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                randomizeColor(v);
-            }
-        });
 
         backgroundImageView.post(new Runnable() {
             @Override
@@ -41,11 +42,12 @@ public class MainActivity extends AppCompatActivity {
         rnd = new Random();
 
         //TODO Show toast that tells the rgb value of a new background color
+        //TODO Confirm that user really wants to change the background
+        //TODO User can restore the previous background
 
     }
 
-    public void randomizeColor(View v)
-    {
+    public void randomizeColor(View v) {
         int red = rnd.nextInt(256);
         int green = rnd.nextInt(256);
         int blue = rnd.nextInt(256);
@@ -55,5 +57,14 @@ public class MainActivity extends AppCompatActivity {
         backgroundImageView.setImageBitmap(backgroundBitmap);
     }
 
+    /** Changes the phone's background.
+     */
+    public void changePhoneBackground(View v) throws IOException {
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+        wallpaperManager.setBitmap(backgroundBitmap);
+    }
 
+    //there might be use for this later:
+    //Intent intent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    //System.out.println(intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0));
 }
